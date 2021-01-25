4b0c1a60b755b1bdd31a05924ce72dcea3d0a2e2
Cyril Mercier <equilix@outlook.fr>
2021-01-25 16:13:02 +0100

    modif mineures

--- Quizz_Models/QuizzBDD.cs
+++ Quizz_Models/QuizzBDD.cs
@@ -7,21 +7,20 @@ using System.Threading.Tasks;
 namespace Quizz_Models
 {
     /*  
-     *  Ajouter les methodes pour interagir avec la base ici et ne pas passer directement par le model.
-     *  Il y a deja des methodes pour servir d'exemple
+     *  Ajouter les methodes qui interagissent avec la bdd et qui on besoin de logique ici.
      */
 
-    public sealed class DAOQuizzBDD
+    public sealed class QuizzBDD
     {
         /* --- Attributs --- */
         bdd_quizz_Entities bdd_entities;        // Reference aux entites de la bdd quizz
-        static DAOQuizzBDD bdd_instance { get { return LazyInstance.Value; } }       // Instance de cette classe 
-        static readonly Lazy<DAOQuizzBDD> LazyInstance = new Lazy<DAOQuizzBDD> (() => new DAOQuizzBDD ());    // Singleton
+        static QuizzBDD bdd_instance { get { return LazyInstance.Value; } }       // Instance de cette classe 
+        static readonly Lazy<QuizzBDD> LazyInstance = new Lazy<QuizzBDD> (() => new QuizzBDD ());    // Singleton
 
 
 
         /* --- Constructeur --- */
-        private DAOQuizzBDD ()
+        private QuizzBDD ()
         {
             bdd_entities = new bdd_quizz_Entities ();
         }
@@ -29,20 +28,22 @@ namespace Quizz_Models
 
 
         /* --- Methodes --- */
-
-
-
         /*  Questions  */
         public question GetQuestionByID ( int prmID )
         {
             return bdd_entities.question.Find (prmID);
         }
 
-        public void InsertQuestion ( question prmQuestion )
+        public void InsertNouvelleQuestion ( question prmQuestion )
         {
             bdd_entities.question.Add (prmQuestion);
         }
 
+        /* Reponse Candidat */
+        public void InsertReponseCandidat ( reponse_candidat prmRepCand )
+        {
+            bdd_entities.reponse_candidat.Add (prmRepCand);
+        }
 
 
         /* Compte */
