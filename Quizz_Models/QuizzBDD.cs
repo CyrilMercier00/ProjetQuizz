ef171dc20c00b4d6f6fa1b85532c74364898e67f
Cyril Mercier <equilix@outlook.fr>
2021-01-26 15:56:21 +0100

    renommer methode

--- Quizz_Models/QuizzBDD.cs
+++ Quizz_Models/QuizzBDD.cs
@@ -52,9 +52,10 @@ namespace Quizz_Models
             return bdd_entities.compte.Find (prmID);
         }
 
-        public void CreerCompte ( compte prmCompte )
+        public void InsertCompte ( compte prmCompte )
         {
             bdd_entities.compte.Add (prmCompte);
         }
+
     }
 }
