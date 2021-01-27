33e6e72fc91c1d41b362798597b079c1b80677b7
Cyril Mercier <equilix@outlook.fr>
2021-01-27 11:24:54 +0100

    Revert "premiere version du front"
    This reverts commit 083946c83756fac5a2ffbef4d398f1b0fa8d7808.

--- Quizz_Web/ClientApp/src/app/app.module.ts
+++ Quizz_Web/ClientApp/src/app/app.module.ts
@@ -10,26 +10,27 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { QuestionCandidat } from './quizz_question/quizz-question.component';
 import { QuizzQuestionComponent } from './quizz-question/quizz-question.component';
 
 @NgModule({
   declarations: [
-    QuizzQuestionComponent,
+    QuestionCandidat,
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
+    QuizzQuestionComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: QuestionCandidat, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'quizz-question', component: QuizzQuestionComponent },
     ])
   ],
   providers: [],
--- Quizz_Web/ClientApp/src/app/quizz-question/quizz-question.component.ts
+++ Quizz_Web/ClientApp/src/app/quizz-question/quizz-question.component.ts
@@ -1,12 +1,10 @@
-import { flatten } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz-question',
   templateUrl: './quizz-question.component.html',
-  styleUrls: ['./quizz-question.component.css', '../app.flex-util.css']
+  styleUrls: ['./quizz-question.component.css']
 })
-
 export class QuizzQuestionComponent implements OnInit {
 
   constructor() { }
