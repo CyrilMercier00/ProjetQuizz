07d0f879da4dc16f610937e46cb69d99b1a35c42
Joris Graaffe <joris.graaffe@gmail.com>
2021-01-28 09:29:14 +0100

    Ajout du dossier DTO (CompteDTO/PermissionDTO)

---
+++ Quizz_Models/DTO/CompteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_Models.DTO
+{
+    class CompteDTO
+    {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string mail { get; set; }
+    }
+}
---
+++ Quizz_Models/DTO/PermissionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_Models.DTO
+{
+    class PermissionDTO
+    {
+        public Nullable<bool> generer_quizz { get; set; }
+        public Nullable<bool> ajouter_quest { get; set; }
+        public Nullable<bool> modifier_quest { get; set; }
+        public Nullable<bool> suppr_question { get; set; }
+    }
+}
