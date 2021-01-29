1a93d8ea8a36f3023ce27c033f6c2fd1975cd5b0
Cyril Mercier <equilix@outlook.fr>
2021-01-29 09:53:55 +0100

    Revert "Ajout cors"
    This reverts commit 8773eb27522a6eeeffb07d53b3fa1260b3e00dfc.

--- Quizz_Web/Startup.cs
+++ Quizz_Web/Startup.cs
@@ -58,18 +58,12 @@ namespace Quizz_Web
                      pattern: "{controller}/{action=Index}/{id?}");
              });
 
-            app.UseCors (x =>
-             { x.AllowAnyOrigin ()
-                .AllowAnyMethod ()
-                .AllowAnyHeader ();
-             });
-
             app.UseSpa (spa =>
              {
-                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                 // see https://go.microsoft.com/fwlink/?linkid=864501
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                 spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "ClientApp";
 
                  if ( env.IsDevelopment () )
                  {
