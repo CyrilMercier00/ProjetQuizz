f072708a05d6349f0fc8f528f58e06f6195669a4
Cyril Mercier <equilix@outlook.fr>
2021-01-29 11:52:59 +0100

    ajout controller vide

---
+++ Quizz_Web/Controllers/ControllerQuizz.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quizz_Models.DTO;
+using Quizz_Models.Services;
+
+namespace Quizz_Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ControllerQuizz : Controller
+    {
+
+        [HttpPost]
+        public void Post([FromBody] QuizzDTO prmQuizzDTO)
+        {
+           
+        }
+    }
+}
