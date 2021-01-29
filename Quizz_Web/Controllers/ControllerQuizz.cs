f6af38eaa14e73734dd422bca40206b127e0603e
Cyril Mercier <equilix@outlook.fr>
2021-01-29 15:53:13 +0100

    avancement post

--- Quizz_Web/Controllers/ControllerQuizz.cs
+++ Quizz_Web/Controllers/ControllerQuizz.cs
@@ -9,14 +9,22 @@ using Quizz_Models.Services;
 namespace Quizz_Web.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route ("[controller]")]
     public class ControllerQuizz : Controller
     {
+        QuizzService servQuizz;
 
         [HttpPost]
-        public void Post([FromBody] QuizzDTO prmQuizzDTO)
+        public void Post ( [FromBody] QuizzDTO prmQuizzDTO )
         {
-           
+            try
+            {
+                servQuizz.GenererQuizz (prmQuizzDTO.nbQuestions, prmQuizzDTO.complexite, prmQuizzDTO.theme, prmQuizzDTO.chrono);
+            }
+            catch ( Exception e )
+            {
+                Console.WriteLine (e.Message);
+            }
         }
     }
 }
