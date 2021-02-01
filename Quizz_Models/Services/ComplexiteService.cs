2e0fd429229c36604c6c74770f8e662058f5b21a
Amine Ettakhchi <amine.ettakhchi@gmail.com>
2021-02-01 14:20:56 +0100

    Ajout service complex

---
+++ Quizz_Models/Services/ComplexiteService.cs
@@ -0,0 +1,21 @@
+﻿using Quizz_Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_Models.Services
+{
+    class ComplexiteService
+    {
+        public void AjouterTauxCoplexite(Taux_complexiteDTO taux_Complexite)
+        {
+
+        }
+
+        public void ModifierVentilation() { }
+
+       // public  O
+    }
+}
