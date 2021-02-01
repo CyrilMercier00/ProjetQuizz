bfba38304b6076538041d1dd070175629d12663c
Cyril Mercier <equilix@outlook.fr>
2021-02-01 14:36:30 +0100

    appel service gen quizz

--- Quizz_Web/Controllers/ControllerQuizz.cs
+++ Quizz_Web/Controllers/ControllerQuizz.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Quizz_Models.DTO;
+using Quizz_Models.Services;
 
 namespace Quizz_Web.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route ("[controller]")]
     public class ControllerQuizz : Controller
     {
+        QuizzService servQuizz = new QuizzService ();
 
         [HttpPost]
-        public void Post([FromBody] QuizzDTO prmQuizzDTO)
+        public void Post ( [FromBody] QuizzDTO prmQuizzDTO )
         {
 
+            servQuizz.GenererQuizz (
+                prmQuizzDTO.NbQuestions,
+                prmQuizzDTO.Complexite, 
+                prmQuizzDTO.Theme, 
+                prmQuizzDTO.Chrono
+                );
         }
     }
 }
