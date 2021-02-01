f6414f2a6998d61283651a956d9b82973a9f7b50
Cyril Mercier <equilix@outlook.fr>
2021-02-01 14:28:55 +0100

    methode inutile enlevée

--- ConsoleApp1/Repositories/QuestionRepository.cs
+++ ConsoleApp1/Repositories/QuestionRepository.cs
@@ -7,20 +7,20 @@ namespace Quizz_Models.Services
 {
     public class QuestionRepository
     {
-        private readonly bdd_quizzContext bdd_entities = new bdd_quizzContext();
-        private readonly ThemeRepository repoTheme = new ThemeRepository();
-        public QuestionRepository() { }
+        private readonly bdd_quizzContext bdd_entities = new bdd_quizzContext ();
+        private readonly ThemeRepository repoTheme = new ThemeRepository ();
+        public QuestionRepository () { }
 
 
         /*  Questions  */
-        public Question GetQuestionByID(int prmID)
+        public Question GetQuestionByID ( int prmID )
         {
-            return bdd_entities.Question.Find(prmID);
+            return bdd_entities.Question.Find (prmID);
         }
 
-        public void InsertNouvelleQuestion(Question prmQuestion)
+        public void InsertNouvelleQuestion ( Question prmQuestion )
         {
-            bdd_entities.Question.Add(prmQuestion);
+            bdd_entities.Question.Add (prmQuestion);
         }
         /// <summary>
         /// Selectionne des questions au hasard dans la base et les ajoute a liste passée.
@@ -29,44 +29,27 @@ namespace Quizz_Models.Services
         /// <param name="prmNBQuestions">Nombre de question a generer pour ces parametres</param>
         /// <param name="prmTheme">Nom du theme des questions (Sensible a la casse)</param>
         /// <param name="prmComplex">Nom du niveau de complexité. (Sensible a la casse)</param>
-        public void GenererQuestions(List<Question> prmListQuestion, int prmNBQuestions, String prmTheme, String prmComplex)
+        public void GenererQuestions ( List<Question> prmListQuestion, int prmNBQuestions, String prmTheme, String prmComplex )
         {
-            int idTheme = repoTheme.GetIDThemeByNom(prmTheme);
+            int idTheme = repoTheme.GetIDThemeByNom (prmTheme);
 
             // Recuperer le nombre de questions total pour ce theme & niv de complexite
             int nbQuestTotal = bdd_entities.Question
-                .Where(x => x.FkTheme == idTheme && x.NvComplexite == prmComplex)
-                .Count();
+                .Where (x => x.FkTheme == idTheme && x.NvComplexite == prmComplex)
+                .Count ();
 
             // Recuper un certain nombre de question pour ce theme & niv de complexite
             var data = bdd_entities.Question
-                .Where(x => x.FkTheme == idTheme && x.NvComplexite == prmComplex)
-                .OrderBy(x => Guid.NewGuid())
-                .Take(prmNBQuestions);
+                .Where (x => x.FkTheme == idTheme && x.NvComplexite == prmComplex)
+                .OrderBy (x => Guid.NewGuid ())
+                .Take (prmNBQuestions);
 
-            foreach (Question q in data)
+            foreach ( Question q in data )
             {
-                prmListQuestion.Add(q);
+                prmListQuestion.Add (q);
             }
-        }
-
-
-        /// <summary>
-        /// Prend fait la liaison entre la table quizz et les questions rentrées
-        /// </summary>
-        /// <param name="prmListQuestion"></param>
-        /// <param name="prmQuizz"></param>
-        public void MAJManyToManyQuest(List<Question> prmListQuestion, Quizz prmQuizz)
-        {
-            foreach (Question q in prmListQuestion)
-            {
-                //bdd_entities.Entry (q)
-                //.Collection (x => x.QuizzQuestion).Load ();
-                //q.QuizzQuestion.Add (prmQuizz);
-                //bdd_entities.SaveChanges ();
-            }
-
         }
 
     }
+
 }
