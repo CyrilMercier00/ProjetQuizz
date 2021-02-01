2fdbfc9b48f5ce81139c60e6c7bba78c0dcd2b90
Cyril Mercier <equilix@outlook.fr>
2021-02-01 14:36:18 +0100

    param public

--- ConsoleApp1/DTO/QuizzDTO.cs
+++ ConsoleApp1/DTO/QuizzDTO.cs
@@ -4,10 +4,10 @@ namespace Quizz_Models.DTO
 {
     public class QuizzDTO
     {
-        int NbQuestions { get; set; }
-        TimeSpan Chrono { get; set; }
-        String Theme { get; set; }
-        String Complexite { get; set; }
+        public int NbQuestions { get; set; }
+        public TimeSpan Chrono { get; set; }
+        public String Theme { get; set; }
+        public String Complexite { get; set; }
 
     }
 }
