1925778a7dde77088fc77b93e11a81f88d12131b
Joris Graaffe <joris.graaffe@gmail.com>
2021-02-02 11:54:49 +0100

    Commit après merge

--- Quizz_Models/DTO/CompteDTO.cs
+++ Quizz_Models/DTO/CompteDTO.cs
@@ -5,7 +5,7 @@
         public string Nom { get; set; }
         public string Prenom { get; set; }
         public string Mail { get; set; }
-
         public string MDP { get; set; }
+        public int Role { get; set; }
     }
 }
