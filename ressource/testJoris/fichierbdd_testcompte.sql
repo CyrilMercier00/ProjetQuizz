354b444d043c9599dcc49ea8cf47caa6dc100fd8
Joris Graaffe <joris.graaffe@gmail.com>
2021-02-02 12:07:15 +0100

    Suppression certains requêtes dans le fichier de test sql

--- ressource/testJoris/fichierbdd_testcompte.sql
+++ ressource/testJoris/fichierbdd_testcompte.sql
@@ -5,10 +5,3 @@ delete from permission where pk_permission >= 0;
 alter table permission auto_increment = 1;
 select * from permission;
 select * from compte;
-insert into permission(generer_quizz, ajouter_quest, modifier_quest, suppr_question)
-	values (true, true, true, true), (true, false, false, false), (false, false, false, false);
-Insert into theme (nom_theme) values ("C#");
-
-SET FOREIGN_KEY_CHECKS = 0;
-alter table theme modify pk_theme int AUTO_INCREMENT;
-SET FOREIGN_KEY_CHECKS = 1;
