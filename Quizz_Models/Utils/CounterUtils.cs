ac9d5efda343b5447d59b1b640594bd649094cd8
Sabrina-spe <77445170+Sabrina-spe@users.noreply.github.com>
2021-02-03 11:23:31 +0100

    Change branch sabrina

---
+++ Quizz_Models/Utils/CounterUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Quizz_Models.Utils
+{
+    class CounterUtils
+    {
+        /// <summary>
+        /// Class qui gerent un chronometre 
+        /// 
+        /// </summary>
+
+        private static Stopwatch stopWatch { get; set; }
+        private TimeSpan Counter_Q { get; set; }
+
+
+
+        private Boolean Isrestart { get; set; }
+        public CounterUtils()
+        {
+            stopWatch = new Stopwatch();
+            Thread.Sleep(10000);
+        }
+
+
+        public void StartCounter()
+        {
+            stopWatch.Start();
+
+        }
+
+        /// <summary>
+        /// methode qui gerent un chronometre 
+        /// 
+        /// </summary>
+        public TimeSpan RestartCounter(DateTime DatetimeSpan)
+        {
+            Counter_Q = DatetimeSpan.TimeOfDay;
+            Counter_Q = stopWatch.Elapsed + Counter_Q;
+            stopWatch.Start();
+            return Counter_Q;
+        }
+        public TimeSpan StopCounter()
+        {
+            stopWatch.Stop();
+            Counter_Q = Counter_Q + stopWatch.Elapsed;
+            return Counter_Q;
+        }
+        public void ResetCounter()
+        {
+            stopWatch.Reset();
+            Counter_Q = stopWatch.Elapsed;
+        }
+
+        public string DisplayCounter()
+        {
+            string DisplayCnt = Convert.ToString(Counter_Q + stopWatch.Elapsed);
+            return DisplayCnt;
+
+        }
+
+
+    }
+}
---
+++ Quizz_Models/Utils/GenerateUrl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+namespace ProjQuizz
+{
+    class GenerateUrl
+    {
+        public GenerateUrl()
+        {
+        }
+
+        /// <summary>
+        /// Genere un code Alphanuerique unique 
+        /// </summary>
+        /// <returns></returns>
+        public string GenerateGuid() 
+        {
+            Guid guid = Guid.NewGuid();
+            string rString = Convert.ToBase64String(guid.ToByteArray());
+            rString = rString.Replace("=", ""); //remove '='
+          //  MessageBox.Show(rString);
+            return rString;
+         
+        }
+        /// <summary>
+        /// supprime les caractere spéciaux 
+        /// </summary>
+        /// <param name="input"> chaine de characters avec characters speciaux</param>
+        /// <returns>chaine de characters sans characters speciaux</returns>
+        public static string RemoveSpecialCharacters(string input)
+        {
+            Regex r = new Regex( "(?:[^a-zA-Z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase| RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            return r.Replace(input, String.Empty);
+        }
+
+        /// <summary>
+        /// Genere url 
+        /// </summary>
+        /// <returns>retourne un code unique pour Url du Quizz</returns>
+        public string GenerateCodeUrl()
+        {
+            string UrlCode = GenerateGuid();
+            UrlCode = RemoveSpecialCharacters(UrlCode);
+            // MessageBox.Show(RemoveSpecialCharacters(rString));
+            return UrlCode;
+         
+        }
+       
+    }
+}
