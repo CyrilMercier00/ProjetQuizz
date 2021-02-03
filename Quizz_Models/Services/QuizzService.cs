b03348475cda401457108a77ea08694dffa8694a
Cyril Mercier <equilix@outlook.fr>
2021-02-03 11:44:55 +0100

    refacto gen question

--- Quizz_Models/Services/QuizzService.cs
+++ Quizz_Models/Services/QuizzService.cs
@@ -37,15 +37,15 @@ namespace Quizz_Models.Services
                 // Gen questions junior
                 repoQuest.GenererQuestions (
                     listQuestionCreation,
-                    CalculerNombreQuestionJunior (prmNBQuestion, TauxComplexite),
+                    CalculerNombreQuestion (prmNBQuestion, Globales.EnumNiveauxComplexiteDispo.junior),
                     ThemeChoisi,
                     Globales.EnumNiveauxComplexiteDispo.junior
                 );
 
                 // Gen questions confirmé
                 repoQuest.GenererQuestions (
-                     listQuestionCreation,
-                    CalculerNombreQuestionConfirme (prmNBQuestion, TauxComplexite),
+                    listQuestionCreation,
+                    CalculerNombreQuestion (prmNBQuestion, Globales.EnumNiveauxComplexiteDispo.confirme),
                     ThemeChoisi,
                     Globales.EnumNiveauxComplexiteDispo.confirme
                 );
@@ -53,7 +53,7 @@ namespace Quizz_Models.Services
                 // Gen questions experimenté
                 repoQuest.GenererQuestions (
                     listQuestionCreation,
-                    CalculerNombreQuestionExperimente (prmNBQuestion, TauxComplexite),
+                    CalculerNombreQuestion (prmNBQuestion, Globales.EnumNiveauxComplexiteDispo.experimenté),
                     ThemeChoisi,
                     Globales.EnumNiveauxComplexiteDispo.experimenté
                 );
@@ -104,9 +104,19 @@ namespace Quizz_Models.Services
 
         public int CalculerNombreQuestion ( int prmNBQuestionTotal, Enum prmNomComplex )
         {
-            int taux = repoComplex.GetTauxComplexiteByNom (prmNomComplex.ToString ());
 
-            return (int) Math.Round (prmNBQuestionTotal / float.Parse ("0." + prmTaux.ToString ()));
+            String complex = prmNomComplex.ToString ().ToLower ();
+
+
+            var valRet = complex switch
+            {
+                "junior" => repoComplex.GetTauxComplexiteByNom (prmNomComplex.ToString ()).QuestionJunior.GetValueOrDefault (),
+                "confirmé" => repoComplex.GetTauxComplexiteByNom (prmNomComplex.ToString ()).QuestionConfirme.GetValueOrDefault (),
+                "experimenté" => repoComplex.GetTauxComplexiteByNom (prmNomComplex.ToString ()).QuestionExperimente.GetValueOrDefault (),
+                _ => throw new Exception ("Le taux de complexitée n'existe pas"),
+            };
+
+            return (int) Math.Round (prmNBQuestionTotal * float.Parse ("0." + valRet.ToString ()));     // Total * 0. valeur dans la bdd
         }
 
     }
