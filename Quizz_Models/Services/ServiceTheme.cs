70d84c1c0f9a10432768800bdb2f691040ec3781
Cyril Mercier <equilix@outlook.fr>
2021-02-05 10:48:35 +0100

    logique get theme

---
+++ Quizz_Models/Services/ServiceTheme.cs
@@ -0,0 +1,22 @@
+﻿using Quizz_Models.bdd_quizz;
+using Quizz_Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizz_Models.Services
+{
+    public class ServiceTheme
+    {
+        readonly ThemeRepository RepoTheme ;
+
+        public ServiceTheme() {
+            RepoTheme = new ThemeRepository();
+        }
+
+        public List<Theme> GetAllThemes ()
+        {
+            return RepoTheme.GetAllTheme();
+        }
+    }
+}
---
+++ Quizz_Web/Controllers/ControllerTheme.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quizz_Models.DTO;
+using Quizz_Models.Services;
+using System;
+
+namespace Quizz_Web.Controllers
+{
+    [ApiController]
+    [Route("api/compte")]
+    public class ControllerTheme : Controller
+    {
+        readonly ServiceTheme servTheme ;
+
+        public ControllerTheme()
+        {
+            this.servTheme = new ServiceTheme();
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            this.servTheme.GetAllThemes();
+        }
+
+    }
+}
