d43560c20dea9a0de3ee8309a2b7fe44c0bcc6e7
Joris Graaffe <joris.graaffe@gmail.com>
2021-02-11 15:41:01 +0100

    Rangement merge

--- Quizz_Web/ClientApp/src/app/compte-feature/compte-feature.module.ts
+++ Quizz_Web/ClientApp/src/app/compte-feature/compte-feature.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ComptesComponent } from './comptes/comptes.component';
 import { CompteComponent } from './compte/compte.component';
 import { CompteRoutingModule } from './compte-routing.module';
-import { BoutonValidationComponent } from './bouton-validation/bouton-validation.component';
-import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     ComptesComponent,
     CompteComponent,
-    InputComponent,
-    BoutonValidationComponent
   ],
   imports: [
     CommonModule,
