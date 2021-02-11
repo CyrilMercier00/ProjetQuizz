9795cf1fbebc15601a0cfc40fd341abd7b042443
Joris Graaffe <joris.graaffe@gmail.com>
2021-02-11 15:16:46 +0100

    Suppression du composant connexion dans le routing et module

--- Quizz_Web/ClientApp/src/app/compte-feature/compte-routing.molule.ts
+++ Quizz_Web/ClientApp/src/app/compte-feature/compte-routing.molule.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { ComptesComponent } from "./comptes/comptes.component";
-import { ConnexionComponent } from "./connexion/connexion.component";
 
 @NgModule({
     imports: [
         RouterModule.forChild([
-            { path: 'comptes', component: ComptesComponent },
-            { path: 'login', component: ConnexionComponent},
+            { path: 'comptes', component: ComptesComponent }
         ])
     ],
     exports: [
