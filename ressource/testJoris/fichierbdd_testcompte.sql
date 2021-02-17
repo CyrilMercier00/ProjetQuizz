78774d333c17a392b5fbb180e68b65bac9db7154
Joris Graaffe <joris.graaffe@gmail.com>
2021-02-17 12:23:36 +0100

    Changement du fichier de test sql pour prendre en compte les noms

--- ressource/testJoris/fichierbdd_testcompte.sql
+++ ressource/testJoris/fichierbdd_testcompte.sql
@@ -3,10 +3,10 @@ delete from compte where pk_compte >= 0;
 ALTER TABLE compte AUTO_INCREMENT = 1;
 delete from permission where pk_permission >= 0;
 alter table permission auto_increment = 1;
-insert into permission(generer_quizz, ajouter_quest, modifier_quest, modifier_compte, suppr_question, supprimer_compte)
-	values (true, true, true, true, true, true), 
-    (true, true, true, false, true, false), 
-    (false, false, false, false, false, false);
+insert into permission(nom, generer_quizz, ajouter_quest, modifier_quest, modifier_compte, suppr_question, supprimer_compte)
+	values ('Administrateur', true, true, true, true, true, true), 
+    ('Recruteur', true, true, true, false, true, false), 
+    ('Candidat', false, false, false, false, false, false);
 
 select * from compte;
 select * from permission;
