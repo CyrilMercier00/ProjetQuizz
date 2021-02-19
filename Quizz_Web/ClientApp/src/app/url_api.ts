11a6b8d3d17c7c41631a6581f42f11b0b08e1b9b
Cyril Mercier <equilix@outlook.fr>
2021-02-19 11:08:08 +0100

    add url question & rep candidat

--- Quizz_Web/ClientApp/src/app/url_api.ts
+++ Quizz_Web/ClientApp/src/app/url_api.ts
@@ -1,7 +1,10 @@
-export class VariableGlobales {
+export class VariableGlobales
+{
   public static apiURL: string = window.location.origin + "/api/";    // Url d'origine (ex: https://localhost/api/)
   public static apiURLCompte = VariableGlobales.apiURL + "compte/";
   public static apiURLTheme = VariableGlobales.apiURL + "theme/";
   public static apiURLComplexite = VariableGlobales.apiURL + "complexite/";
   public static apiURLQuizz = VariableGlobales.apiURL + "quizz/";
+  public static apiURLQuestion = VariableGlobales.apiURL + "question/";
+  static apiURLReponseCandidat = VariableGlobales.apiURL + "reponsecandidat";
 }
