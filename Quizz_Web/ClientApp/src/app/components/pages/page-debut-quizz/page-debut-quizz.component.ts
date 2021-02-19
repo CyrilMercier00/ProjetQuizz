c894f43f30834066f2902b6e3b72d0d9b3ead778
Cyril Mercier <equilix@outlook.fr>
2021-02-19 11:36:58 +0100

    add page debut quizz

--- Quizz_Web/ClientApp/src/app/components/pages/page-debut-quizz/page-debut-quizz.component.ts
+++ Quizz_Web/ClientApp/src/app/components/pages/page-debut-quizz/page-debut-quizz.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-page-debut-quizz',
   templateUrl: './page-debut-quizz.component.html',
-  styleUrls: ['./page-debut-quizz.component.css']
+  styleUrls: ['./page-debut-quizz.component.css', '../../../app.flex-util.css']
 })
+
+
+
 export class PageDebutQuizzComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  handleClick() {
+    this.router.navigate([''])
   }
 
 }
