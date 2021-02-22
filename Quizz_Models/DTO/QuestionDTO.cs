f5c7d77949ed5991ad49a07835850f9559e12068
Cyril Mercier <equilix@outlook.fr>
2021-02-22 11:28:00 +0100

    crea dto

---
+++ Quizz_Models/DTO/QuestionDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Quizz_Models.DTO
+{
+    public class QuestionDTO
+    {
+        public String Enonce { get; set; }
+        public bool RepLibre { get; set; }
+        public String FKComplexite { get; set; }
+        public String FKTheme { get; set; }
+
+    }
+}
