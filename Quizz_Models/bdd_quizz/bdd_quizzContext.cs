2c1e981e933bbea2279f6bed3c4b69c7addf3a9d
Joris Graaffe <joris.graaffe@gmail.com>
2021-02-25 15:01:38 +0100

    Enlever le warnaing

--- Quizz_Models/bdd_quizz/bdd_quizzContext.cs
+++ Quizz_Models/bdd_quizz/bdd_quizzContext.cs
@@ -30,7 +30,6 @@ namespace Quizz_Models.bdd_quizz
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=root;database=bdd_quizz");
             }
         }
