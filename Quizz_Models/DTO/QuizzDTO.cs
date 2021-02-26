0a970918f5966f12c78721dc2469929679b26190
Cyril Mercier <equilix@outlook.fr>
2021-02-26 14:53:05 +0100

    fix dto

--- Quizz_Models/DTO/QuizzDTO.cs
+++ Quizz_Models/DTO/QuizzDTO.cs
@@ -5,6 +5,7 @@ namespace Quizz_Models.DTO
     public class QuizzDTO
     {
         public int PkQuizz { get; set; }
+        public int FKCompteRecruteur { get; set; }
         public int NbQuestions { get; set; }
         public String Chrono { get; set; }
         public String Theme { get; set; }
