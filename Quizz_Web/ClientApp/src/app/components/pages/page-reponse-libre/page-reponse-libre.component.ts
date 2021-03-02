8a7fc6cec7447a0153b862af706cfc0348bbe696
Cyril Mercier <equilix@outlook.fr>
2021-03-02 12:18:27 +0100

    Update page-reponse-libre.component.ts

--- Quizz_Web/ClientApp/src/app/components/pages/page-reponse-libre/page-reponse-libre.component.ts
+++ Quizz_Web/ClientApp/src/app/components/pages/page-reponse-libre/page-reponse-libre.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { questionDTO } from 'src/app/DTO/questionDTO';
-
 @Component({
   selector: 'app-page-reponse-libre',
   templateUrl: './page-reponse-libre.component.html',
