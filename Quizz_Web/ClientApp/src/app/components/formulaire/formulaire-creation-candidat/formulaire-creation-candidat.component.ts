4fed16d737b0d4bfe85693e702908544a1e356c5
Amine Ettakhchi <amine.ettakhchi@gmail.com>
2021-03-02 11:01:46 +0100

    form candidat

--- Quizz_Web/ClientApp/src/app/components/formulaire/formulaire-creation-candidat/formulaire-creation-candidat.component.ts
+++ Quizz_Web/ClientApp/src/app/components/formulaire/formulaire-creation-candidat/formulaire-creation-candidat.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Compte } from 'src/app/compte-feature/Compte/compte.model';
 
 @Component({
   selector: 'app-formulaire-creation-candidat',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulaire-creation-candidat.component.css']
 })
 export class FormulaireCreationCandidatComponent implements OnInit {
+  @Output() compteToSend = new EventEmitter<Compte>();
+  @Input() inOut;
+  candidatProfile: FormGroup;
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) {
+    this.candidatProfile = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      mail: ['', Validators.required],
+    
+    });
+   }
   ngOnInit() {
   }
-
+  onSubmit(): void{
+    this.inOut = false;
+    //this.candidatProfile.patchValue({role: 3});
+    this.compteToSend.emit(this.candidatProfile.value);
+  }
 }
