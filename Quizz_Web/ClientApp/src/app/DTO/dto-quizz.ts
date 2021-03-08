0b315f4df78ede38edbd38072e90e9ccbe1da7dc
Cyril Mercier <equilix@outlook.fr>
2021-03-08 12:24:35 +0100

    fix dto

--- Quizz_Web/ClientApp/src/app/DTO/dto-quizz.ts
+++ Quizz_Web/ClientApp/src/app/DTO/dto-quizz.ts
@@ -8,111 +8,100 @@ export class DTOQuizz
   private Theme: string;
   private Complexite: string;
 
-  /**
-   * Getter $UrlCode
-   * @return {string}
-   */
-  public get $UrlCode(): string
-  {
-    return this.UrlCode;
-  }
+    /**
+     * Getter $UrlCode
+     * @return {string}
+     */
+	public get $UrlCode(): string {
+		return this.UrlCode;
+	}
 
-  /**
-   * Setter $UrlCode
-   * @param {string} value
-   */
-  public set $UrlCode(value: string)
-  {
-    this.UrlCode = value;
-  }
+    /**
+     * Getter $FKCompteRecruteur
+     * @return {number}
+     */
+	public get $FKCompteRecruteur(): number {
+		return this.FKCompteRecruteur;
+	}
 
-  /**
-   * Getter $FKCompteRecruteur
-   * @return {number}
-   */
-  public get $FKCompteRecruteur(): number
-  {
-    return this.FKCompteRecruteur;
-  }
+    /**
+     * Getter $NbQuestions
+     * @return {number}
+     */
+	public get $NbQuestions(): number {
+		return this.NbQuestions;
+	}
 
-  /**
-   * Getter $NbQuestions
-   * @return {number}
-   */
-  public get $NbQuestions(): number
-  {
-    return this.NbQuestions;
-  }
+    /**
+     * Getter $Chrono
+     * @return {string}
+     */
+	public get $Chrono(): string {
+		return this.Chrono;
+	}
 
-  /**
-   * Getter $Chrono
-   * @return {string}
-   */
-  public get $Chrono(): string
-  {
-    return this.Chrono;
-  }
+    /**
+     * Getter $Theme
+     * @return {string}
+     */
+	public get $Theme(): string {
+		return this.Theme;
+	}
 
-  /**
-   * Getter $Theme
-   * @return {String}
-   */
-  public get $Theme(): String
-  {
-    return this.Theme;
-  }
+    /**
+     * Getter $Complexite
+     * @return {string}
+     */
+	public get $Complexite(): string {
+		return this.Complexite;
+	}
 
-  /**
-   * Getter Complexite
-   * @return {String}
-   */
-  public get $Complexite(): String
-  {
-    return this.Complexite;
-  }
+    /**
+     * Setter $UrlCode
+     * @param {string} value
+     */
+	public set $UrlCode(value: string) {
+		this.UrlCode = value;
+	}
 
-  /**
-   * Setter $FKCompteRecruteur
-   * @param {number} value
-   */
-  public set $FKCompteRecruteur(value: number)
-  {
-    this.FKCompteRecruteur = value;
-  }
+    /**
+     * Setter $FKCompteRecruteur
+     * @param {number} value
+     */
+	public set $FKCompteRecruteur(value: number) {
+		this.FKCompteRecruteur = value;
+	}
 
-  /**
-   * Setter $NbQuestions
-   * @param {number} value
-   */
-  public set $NbQuestions(value: number)
-  {
-    this.NbQuestions = value;
-  }
+    /**
+     * Setter $NbQuestions
+     * @param {number} value
+     */
+	public set $NbQuestions(value: number) {
+		this.NbQuestions = value;
+	}
 
-  /**
-   * Setter $Chrono
-   * @param {string} value
-   */
-  public set $Chrono(value: string)
-  {
-    this.Chrono = value;
-  }
+    /**
+     * Setter $Chrono
+     * @param {string} value
+     */
+	public set $Chrono(value: string) {
+		this.Chrono = value;
+	}
 
-  /**
-   * Setter $Theme
-   * @param {String} value
-   */
-  public set $Theme(value: String)
-  {
-    this.Theme = value;
-  }
+    /**
+     * Setter $Theme
+     * @param {string} value
+     */
+	public set $Theme(value: string) {
+		this.Theme = value;
+	}
 
-  /**
-   * Setter Complexite
-   * @param {String} value
-   */
-  public set $Complexite(value: String)
-  {
-    this.Complexite = value;
-  }
+    /**
+     * Setter $Complexite
+     * @param {string} value
+     */
+	public set $Complexite(value: string) {
+		this.Complexite = value;
+	}
+
 }
