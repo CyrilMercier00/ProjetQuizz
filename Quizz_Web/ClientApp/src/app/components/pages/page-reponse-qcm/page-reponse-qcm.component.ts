616cdd0988db2f2962bbe9a9bea886d5cc964047
Cyril Mercier <equilix@outlook.fr>
2021-03-15 10:42:58 +0100

    avancement marche

--- Quizz_Web/ClientApp/src/app/components/pages/page-reponse-qcm/page-reponse-qcm.component.ts
+++ Quizz_Web/ClientApp/src/app/components/pages/page-reponse-qcm/page-reponse-qcm.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { DTOQuestion } from 'src/app/DTO/questionDTO';
-import { reponseDTO } from '../../../DTO/reponseDTO';
+import { ReponseCandidatDTO } from '../../../DTO/ReponseCandidatDTO';
+import { serviceRepCandidat } from 'src/app/Service/serviceRepCandidat';
 import { utils } from 'src/app/utils';
 
 @Component({
@@ -27,7 +28,6 @@ export class PageReponseQcmComponent implements OnInit
   rep4: string;               // Texte de la réponse 4
   enonce: string;             // Enonce de la question
   textCommentaire: string;    // Commentaire du candidat
-  aRepondu: boolean;          // Si on a repondu a la question
 
 
   /* ------ Constructeur ------ */
@@ -47,26 +47,35 @@ export class PageReponseQcmComponent implements OnInit
     let arrayNbChoisis = utils.nbAleatUnique(4, 0, this.dataQ.$ListeReponses.length)
 
     // Utiliser ces nombres pour afficher le questions
-    this.rep1 == this.dataQ.$ListeReponses[arrayNbChoisis[0]];
-    this.rep2 == this.dataQ.$ListeReponses[arrayNbChoisis[1]];
-    this.rep3 == this.dataQ.$ListeReponses[arrayNbChoisis[2]];
-    this.rep4 == this.dataQ.$ListeReponses[arrayNbChoisis[3]];
+    /*
+    this.rep1 = this.dataQ.$ListeReponses[arrayNbChoisis[0]];
+    this.rep2 = this.dataQ.$ListeReponses[arrayNbChoisis[1]];
+    this.rep3 = this.dataQ.$ListeReponses[arrayNbChoisis[2]];
+    this.rep4 = this.dataQ.$ListeReponses[arrayNbChoisis[3]];
+    */
+    this.rep1 = this.dataQ.$ListeReponses[0].Text;
+    this.rep2 = this.dataQ.$ListeReponses[1].Text;
+    this.rep3 = this.dataQ.$ListeReponses[2].Text;
+    this.rep4 = this.dataQ.$ListeReponses[3].Text;
+
   }
 
 
 
   /* ------ Methodes ------*/
-  handleClick (event)
+  GetClick(idRep)
   {
-    let data = new reponseDTO();
+    console.log("cc")
+    let dtoRep = new ReponseCandidatDTO()
 
-    data.$Commentaire = this.textCommentaire;
-    data.$Reponse = event.target.value;
-    data.$FKCompte = 0;
-    data.$FKQuestion = this.dataQ.$PKQuestion;
+    dtoRep.$Commentaire = this.textCommentaire;
+    dtoRep.$Reponse = this.dataQ.$ListeReponses[idRep];
+    dtoRep.$FKCompte = null;     // TODO : Get compte candidat qui passe
+    dtoRep.$FKQuestion = this.dataQ.$PKQuestion;
+
+    serviceRepCandidat.PostReponse(dtoRep)
 
     this.estRepondu.emit(true);
 
   }
-
 }
