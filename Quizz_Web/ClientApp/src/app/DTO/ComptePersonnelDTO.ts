1627b0cd14045972292e6eff62c3894b40488888
Cyril Mercier <equilix@outlook.fr>
2021-03-15 16:29:18 +0100

    add champ pk

--- Quizz_Web/ClientApp/src/app/DTO/ComptePersonnelDTO.ts
+++ Quizz_Web/ClientApp/src/app/DTO/ComptePersonnelDTO.ts
@@ -1,26 +1,51 @@
 import { PermissionDTO } from "./PermissionDTO";
 
-export class ComptePersonnelDTO {
-    private nom    : string;
-    private prenom    : string;
-    private permissionDTO   : PermissionDTO;
+export class ComptePersonnelDTO
+{
+  private nom: string;
+  private prenom: string;
+  private permissionDTO: PermissionDTO;
+  private PkQuizz: number;
 
-    constructor(nom : string, prenom : string, permissionDTO : PermissionDTO){
+  constructor(nom: string, prenom: string, permissionDTO: PermissionDTO)
+  {
 
-        this.nom = nom;
-        this.prenom = prenom;
-        this.permissionDTO = permissionDTO;
-    }
+    this.nom = nom;
+    this.prenom = prenom;
+    this.permissionDTO = permissionDTO;
+  }
 
-    public get GetNom(): string{
-        return this.nom;
-    }
+  public get GetNom(): string
+  {
+    return this.nom;
+  }
 
-    public get GetPrenom(): string{
-        return this.prenom;
-    }
-    
-    public get GetPermissionDTO(): PermissionDTO{
-        return this.permissionDTO;
-    }
+  public get GetPrenom(): string
+  {
+    return this.prenom;
+  }
+
+  public get GetPermissionDTO(): PermissionDTO
+  {
+    return this.permissionDTO;
+  }
+
+
+  /**
+   * Getter $PkQuizz
+   * @return {number}
+   */
+  public get $PkQuizz(): number
+  {
+    return this.PkQuizz;
+  }
+
+  /**
+   * Setter $PkQuizz
+   * @param {number} value
+   */
+  public set $PkQuizz(value: number)
+  {
+    this.PkQuizz = value;
+  }
 }
