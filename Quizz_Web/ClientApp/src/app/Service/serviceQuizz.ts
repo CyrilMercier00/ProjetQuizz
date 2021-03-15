86eb5d2896c1fb43d0ad4ada7fe2bf585e14a8ba
Cyril Mercier <equilix@outlook.fr>
2021-03-15 13:31:32 +0100

    add CYERJETON

--- Quizz_Web/ClientApp/src/app/Service/serviceQuizz.ts
+++ Quizz_Web/ClientApp/src/app/Service/serviceQuizz.ts
@@ -1,9 +1,7 @@
-import { VariableGlobales } from "../url_api";
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
 import { DTOQuizz } from "../DTO/dto-quizz";
 import { Globals } from "../globals";
+import { Injectable } from '@angular/core';
+import { VariableGlobales } from "../url_api";
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +12,26 @@ export class ServiceQuizz
   // * Retoune les details du quizz avec le code unique pa
   public static GetQuizzByCode(prmCode: string): Promise<Response>
   {
-    return fetch(VariableGlobales.apiURLQuizz + prmCode, { method: "GET" })
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+    requestHeaders.set('Authorization', Globals.getJwt());
+
+    return fetch(VariableGlobales.apiURLQuizz + prmCode, { method: "GET", headers: requestHeaders });
+
   }
 
-  // * Retoune les details du quizz avec le code unique pa
+
+
+  // * Retoune les details du quizz avec le code
   public static UpdateQuizz(prmCode: string): Promise<Response>
   {
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+    requestHeaders.set('Authorization', Globals.getJwt());
+
     this.GetQuizzByCode(prmCode);
-    return fetch(VariableGlobales.apiURLQuizz + prmCode, { method: "PUT" })
+
+    return fetch(VariableGlobales.apiURLQuizz + prmCode, { method: "PUT", headers: requestHeaders })
   }
 
 
@@ -29,9 +39,15 @@ export class ServiceQuizz
   // * Retourne la route de validation du Quizz
   public static ValidateQuizz(prmCodeQuizz: string): Promise<Response>
   {
-    let jwt = Globals.decodeJwt();    
-    return fetch(VariableGlobales.apiURLFinQuizz+ prmCodeQuizz+ "/" + jwt['id'],{ method: "GET" })
+    let jwt = Globals.decodeJwt();
+
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+    requestHeaders.set('Authorization', Globals.getJwt());
+
+    return fetch(VariableGlobales.apiURLFinQuizz + prmCodeQuizz + "/" + jwt['id'], { method: "GET", headers: requestHeaders })
   }
+
 
 
   public static InsertQuizz(prmDTO: DTOQuizz): Promise<Response>
@@ -43,7 +59,8 @@ export class ServiceQuizz
         headers:
         {
           'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': Globals.getJwt()
         },
         body: JSON.stringify(prmDTO)
       }
