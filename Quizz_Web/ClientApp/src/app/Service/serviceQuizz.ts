1a4fe822cf324b3c69f4f977a8bc988c729d7c68
Cyril Mercier <equilix@outlook.fr>
2021-03-16 10:12:14 +0100

    update route

--- Quizz_Web/ClientApp/src/app/Service/serviceQuizz.ts
+++ Quizz_Web/ClientApp/src/app/Service/serviceQuizz.ts
@@ -16,7 +16,7 @@ export class ServiceQuizz
     requestHeaders.set('Content-Type', 'application/json');
     requestHeaders.set('Authorization', Globals.getJwt());
 
-    return fetch(VariableGlobales.apiURLQuizz + prmCode, { method: "GET", headers: requestHeaders });
+    return fetch(VariableGlobales.apiURLQuizz + "null/" + prmCode, { method: "GET", headers: requestHeaders });
 
   }
 
