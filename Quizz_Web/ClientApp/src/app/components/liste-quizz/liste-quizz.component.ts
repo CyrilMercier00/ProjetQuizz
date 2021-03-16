87ee5712503ebe815bcdba7f22127e30d0d49c8e
Joris Graaffe <joris.graaffe@gmail.com>
2021-03-16 11:51:25 +0100

    Tentative de retour quizz liste

--- Quizz_Web/ClientApp/src/app/components/liste-quizz/liste-quizz.component.ts
+++ Quizz_Web/ClientApp/src/app/components/liste-quizz/liste-quizz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from 'src/app/globals';
+import { VariableGlobales } from 'src/app/url_api';
 
 @Component({
   selector: 'app-liste-quizz',
@@ -10,6 +12,20 @@ export class ListeQuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.initListeQuizz();
   }
 
+  private initListeQuizz(): void {
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+    requestHeaders.set('Authorization', Globals.getJwt());
+
+    fetch(
+      VariableGlobales.apiURLQuizz + Globals.getId(),
+      {
+        method: "GET",
+        headers: requestHeaders
+      }
+    ).then(response => console.log(response.json()));
+  }
 }
