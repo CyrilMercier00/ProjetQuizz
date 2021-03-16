f6d0c79fac6fa059b436fbe3cac0e2cf817b2b61
Joris Graaffe <joris.graaffe@gmail.com>
2021-03-16 11:23:53 +0100

    Rennomage du composant liste-quizz

---
+++ Quizz_Web/ClientApp/src/app/components/liste-quizz/liste-quizz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-quizz',
+  templateUrl: './liste-quizz.component.html',
+  styleUrls: ['./liste-quizz.component.css']
+})
+export class ListeQuizzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
