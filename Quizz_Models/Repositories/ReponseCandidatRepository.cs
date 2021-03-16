192339b12ad0d840716c8818be9365ce8bb10d24
Sabrina-spe <77445170+Sabrina-spe@users.noreply.github.com>
2021-03-16 11:27:45 +0100

    modif suite merge

--- Quizz_Models/Repositories/ReponseCandidatRepository.cs
+++ Quizz_Models/Repositories/ReponseCandidatRepository.cs
@@ -14,10 +14,10 @@ namespace Quizz_Models.Repositories
             bdd_entities = context;
         }
 
-        public void InsertReponseCandidat(ReponseCandidat prmReponseCandidat)
+        public int InsertReponseCandidat(ReponseCandidat prmReponseCandidat)
         {
             bdd_entities.ReponseCandidat.Add(prmReponseCandidat);
-            bdd_entities.SaveChanges();
+            return bdd_entities.SaveChanges();
         }
 
         public List<ReponseCandidat> SelectReponseCandidatByIDQuestion(int prmIDQuestion)
