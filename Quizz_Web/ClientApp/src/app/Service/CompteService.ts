a2cdb440e36f8e41ae7e5e0913ce693cae7db0aa
Joris Graaffe <joris.graaffe@gmail.com>
2021-03-17 12:28:57 +0100

    Tentative

--- Quizz_Web/ClientApp/src/app/Service/CompteService.ts
+++ Quizz_Web/ClientApp/src/app/Service/CompteService.ts
@@ -11,22 +11,15 @@ export class CompteService {
   constructor() { }
 
   public static GetCompteLinkedToCode(prmCode: string): Promise<Response> {
-    let jwt: string;
-
-    if (Globals.isLoggedIn()) 
+    if (!Globals.isLoggedIn()) 
     {
-      console.log("1")
-      jwt = Globals.decodeJwt();
-    } else {
-      console.log("2")
-      jwt = '';
+      Globals.initJwt('');
     }
 
     const requestHeaders: HeadersInit = new Headers();
     requestHeaders.set('Content-Type', 'application/json');
     requestHeaders.set('Authorization', Globals.getJwt());
-    console.log(VariableGlobales.apiURLCompte + "kko/" + prmCode + "/deconnecte", { method: "GET", headers: requestHeaders })
-    return fetch(VariableGlobales.apiURLCompte + "kko/" + prmCode + "/deconnecte", { method: "GET", headers: requestHeaders })
+    return fetch(VariableGlobales.apiURLCompte + "kko/" + prmCode + "/deconnecte", { method: "GET"});
 
   }
 }
